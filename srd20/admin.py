14b72908d94b60929b927ef310a7416324e460dc
Daniel F. Moisset <dmoisset@machinalis.com>
2011-01-30 08:32:28 -0300

    Minor changes in admin

--- srd20/admin.py
+++ srd20/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from srd20.models import Spell
 
 class SpellAdmin(admin.ModelAdmin):
-    list_display= ('name', 'level', 'short_description')
+    list_display = ('name', 'level', 'short_description')
+    list_filter = ('school',)
     search_fields = ('name',)
 
 admin.site.register(Spell, SpellAdmin)
