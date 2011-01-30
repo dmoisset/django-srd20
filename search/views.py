af990d720746cf5b789afd739957e3c72e5a86c4
Daniel F. Moisset <dmoisset@machinalis.com>
2011-01-30 08:50:28 -0300

    Implemented full text search

--- search/views.py
+++ search/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+from django.db.models import Q
 from .forms import SearchForm
 from srd20.models import Spell
 
@@ -10,7 +11,12 @@ def search_results(request):
     if 'q' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
-            results = Spell.objects.filter(name__icontains=form.cleaned_data['q'])
+            query_string = form.cleaned_data['q']
+            query = Q(name__icontains=query_string)
+            if form.cleaned_data['search_type'] == 'all':
+                for field in ('altname', 'short_description', 'description', 'school', 'subschool', 'descriptor', 'material_components', 'reference'):
+                    query |= Q(**{field+'__icontains': query_string})
+            results = Spell.objects.filter(query)
     else:
         form = SearchForm()
         results = None
