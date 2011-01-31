6740618b06fa99b34b043068e897d36797b6ecb1
Daniel F. Moisset <dmoisset@machinalis.com>
2011-01-30 23:29:15 -0300

    Removed help comments added by django

--- srd20/models.py
+++ srd20/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Spell(models.Model):
     name = models.CharField(max_length=64)
     altname = models.CharField(max_length=64)
--- urls.py
+++ urls.py
@@ -1,18 +1,10 @@
 from django.conf.urls.defaults import *
 import browse.urls
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^django_srd20/', include('django_srd20.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
     (r'^browse/', include(browse.urls)),
     (r'^$', 'search.views.search_start'),
