4f86ce950efb499edc3cb773dc0c4b6f68d077cb
Daniel F. Moisset <dmoisset@machinalis.com>
2011-01-30 23:22:04 -0300

    Minor refactor

--- search/views.py
+++ search/views.py
@@ -7,6 +7,18 @@ from srd20.models import Spell
 def search_start(request):
     return render_to_response('search/start.html', {'form': SearchForm()})
 
+# These are the fields searched by the full text search
+SEARCH_FIELDS = (
+    'altname',
+    'short_description',
+    'description',
+    'school',
+    'subschool',
+    'descriptor',
+    'material_components',
+    'reference',
+)
+
 def search_results(request):
     if 'q' in request.GET:
         form = SearchForm(request.GET)
@@ -14,7 +26,7 @@ def search_results(request):
             query_string = form.cleaned_data['q']
             query = Q(name__icontains=query_string)
             if form.cleaned_data['search_type'] == 'all':
-                for field in ('altname', 'short_description', 'description', 'school', 'subschool', 'descriptor', 'material_components', 'reference'):
+                for field in SEARCH_FIELDS:
                     query |= Q(**{field+'__icontains': query_string})
             results = Spell.objects.filter(query)
     else:
