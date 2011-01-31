a5fd2c60a455d9b25f32dbbe9d2dc3ee2d0f4564
Daniel F. Moisset <dmoisset@machinalis.com>
2011-01-30 23:11:11 -0300

    Added edit link

--- browse/views.py
+++ browse/views.py
@@ -3,5 +3,8 @@ from srd20.models import Spell
 
 def spell_detail(request, id):
     spell = get_object_or_404(Spell, pk=id)
-    return render_to_response('browse/spell.html', {'spell': spell})
+    return render_to_response('browse/spell.html', {
+        'spell': spell,
+        'editable': request.user.has_perm('srd20.can_edit_spell'),
+    })
 
