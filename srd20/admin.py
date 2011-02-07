0903e5eb22f2a0d5b06830645e2637a4a230be41
Daniel F. Moisset <dmoisset@machinalis.com>
2011-02-07 01:07:11 -0300

    Javascripto auto populate the altname

--- srd20/admin.py
+++ srd20/admin.py
@@ -5,5 +5,6 @@ class SpellAdmin(admin.ModelAdmin):
     list_display = ('name', 'level', 'short_description')
     list_filter = ('school',)
     search_fields = ('name',)
+    prepopulated_fields = {'altname': ('name',)}
 
 admin.site.register(Spell, SpellAdmin)
