b2b33a6833beadbffc39af35106e9e7ae3e2d2bf
Daniel F. Moisset <dmoisset@machinalis.com>
2012-03-19 21:19:24 -0300

    Some fixes in module names after renames

--- urls.py
+++ urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import *
 import browse.urls
+import search.search_site
 
 from django.contrib import admin
 admin.autodiscover()
-import search.search
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^browse/', include(browse.urls)),
     (r'^$', 'search.views.search_start'),
-    (r'^search/', include(search.search.urls)),
+    (r'^search/', include(search.search_site.urls)),
 )
