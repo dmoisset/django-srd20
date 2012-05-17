35f4de8869bdaf9f0d19d7604eaad0771de121b7
Daniel F. Moisset <dmoisset@machinalis.com>
2012-05-17 19:08:37 -0300

    More fields widened...

--- srd20/models.py
+++ srd20/models.py
@@ -183,7 +183,7 @@ class Monster(models.Model):
     damage_reduction_amount = models.PositiveIntegerField(default=0)
     damage_reduction_condition = models.CharField(max_length=128, blank=True)
     immunities = models.CharField(max_length=512, blank=True)
-    resistance = models.CharField(max_length=128, blank=True)
+    resistance = models.CharField(max_length=256, blank=True)
     spell_resistance = models.PositiveIntegerField(default=0)
     weaknesses = models.CharField(max_length=256, blank=True)
 
